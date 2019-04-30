8c2d2e0b1b5cfecd382d2144d43085829ddbe9f0
BorovkovSergey <707grays@gmail.com>
2019-04-30 18:37:48 +0700

    FIXED ip adresses parser

--- parser.sh
+++ parser.sh
@@ -15,16 +15,18 @@ fi
 echo
 
 grep "ASA" log.txt > uniq_adreses
-grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' uniq_adreses | sort | uniq > uniq_adreses2 
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,999\}' uniq_adreses | sort | uniq > uniq_adreses2 
 LEGAL_IPS_COUNT=`grep -c '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' uniq_adreses2`
-echo "Количество различных IP-адресов, встретившихся в сообщениях Cisco ASA: " ${LEGAL_IPS_COUNT}
+UNLEGAL_IPS_3=`grep -c '[3-9]\{1,1\}[0-9]\{2,2\}' uniq_adreses2`
+UNLEGAL_IPS_3v2=`grep -c '\([2]\{1,1\}[6-9]\{1,1\}[0-9]\{1,9\}\|[2]\{1,1\}[5]\{1,1\}[6-9]\{1,9\}\)' uniq_adreses2`
+grep '[3-9]\{1,1\}[0-9]\{2,2\}' uniq_adreses2 > uniq_adreses4
+grep '\([2]\{1,1\}[6-9]\{1,1\}[0-9]\{1,1\}\|[2]\{1,1\}[5]\{1,1\}[6-9]\{1,1\}\)' uniq_adreses2 > uniq_adreses5
+echo "Количество различных IP-адресов, встретившихся в сообщениях Cisco ASA: " $((${LEGAL_IPS_COUNT}-${UNLEGAL_IPS_3}-${UNLEGAL_IPS_3v2}))
 if [[ $1 && $1 = 'show_all' ]] ;then
     echo "Перечень различных IP-адресов, встретившихся в сообщениях Cisco ASA: " `cat uniq_adreses2 | sort`
 fi
 
-grep -o '[0-9]\{1,999\}\.[0-9]\{1,999\}\.[0-9]\{1,999\}\.[0-9]\{4,999\}' uniq_adreses | sort | uniq > uniq_adreses2 
-UNLEGAL_IPS_COUNT=`grep -c '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' uniq_adreses2`
-echo "Количество различных невалидных IP-адресов, встретившихся в сообщениях Cisco ASA: " ${UNLEGAL_IPS_COUNT}
+echo "Количество различных невалидных IP-адресов, встретившихся в сообщениях Cisco ASA: " $(($UNLEGAL_IPS_3+${UNLEGAL_IPS_3v2}))
 if [[ $1 && $1 = 'show_all' ]] ;then
-    echo "Перечень различных невалидных IP-адресов, встретившихся в сообщениях Cisco ASA: " `cat uniq_adreses2 | sort`
+    echo "Перечень различных невалидных IP-адресов, встретившихся в сообщениях Cisco ASA: " `cat uniq_adreses5 | sort` `cat uniq_adreses4 | sort`
 fi
